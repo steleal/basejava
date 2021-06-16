bf0b8af4231eb7c4cf567b3679c3f4e5f5a52b84
Alexey Shtele <aleksey.shtele@rt.ru>
2021-06-16 14:36:35 +0400

    feature: implement of ArrayStorage.save()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,12 +2,19 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final static int STORAGE_SIZE = 10000;
+
+    private Resume[] storage = new Resume[STORAGE_SIZE];
+    private int resumeCnt = 0;
 
     void clear() {
     }
 
     void save(Resume r) {
+        if (resumeCnt == STORAGE_SIZE) {
+            throw new RuntimeException("The number of resumes is more than the storage size");
+        }
+        storage[resumeCnt++] = r;
     }
 
     Resume get(String uuid) {
