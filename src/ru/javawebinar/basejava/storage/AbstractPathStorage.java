236783802c580a68bd571916e4f395b9494bd119
Alexey Shtele <aleksey.shtele@rt.ru>
2021-07-26 12:44:03 +0400

    Lesson09 AbstractPathStorage

---
+++ src/ru/javawebinar/basejava/storage/AbstractPathStorage.java
@@ -0,0 +1,110 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.StorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * gkislin
+ * 22.07.2016
+ */
+public abstract class AbstractPathStorage extends AbstractStorage<Path> {
+    private final Path directory;
+
+    protected AbstractPathStorage(String dir) {
+        directory = Paths.get(dir);
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!Files.isDirectory(directory) || !Files.isWritable(directory)) {
+            throw new IllegalArgumentException(dir + " is not readable/writable");
+        }
+    }
+
+    @Override
+    public void clear() {
+        dirFiles().forEach(this::doDelete);
+    }
+
+    @Override
+    public int size() {
+        return (int) dirFiles().count();
+    }
+
+    @Override
+    protected Path getSearchKey(String uuid) {
+        return Paths.get(directory.toAbsolutePath().toString(), uuid);
+    }
+
+    @Override
+    protected boolean isExist(Path path) {
+        return Files.exists(path);
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Path path) {
+        try {
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("File write error", r.getUuid(), e);
+        }
+    }
+
+    @Override
+    protected void doSave(Resume r, Path path) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new StorageException("Couldn't create file " + path.toAbsolutePath().toString(), r.getUuid(), e);
+        }
+        doUpdate(r, path);
+    }
+
+    @Override
+    protected Resume doGet(Path path) {
+        try {
+            return doRead(new BufferedInputStream(new FileInputStream(path.toFile())));
+        } catch (IOException e) {
+            throw new StorageException("File read error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected void doDelete(Path path) {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            throw new StorageException("File delete error", path.getFileName().toString(), e);
+        }
+    }
+
+    @Override
+    protected List<Resume> doCopyAll() {
+        return dirFiles().map(this::doGet).collect(Collectors.toList());
+    }
+
+    protected abstract void doWrite(Resume r, OutputStream os) throws IOException;
+
+    protected abstract Resume doRead(InputStream is) throws IOException;
+
+    private Stream<Path> dirFiles() {
+        try {
+            return Files.list(directory).filter(Files::isRegularFile);
+        } catch (IOException e) {
+            throw new StorageException("Directory read error", null);
+        }
+    }
+
+}
