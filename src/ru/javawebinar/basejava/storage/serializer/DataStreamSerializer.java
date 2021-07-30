daff3ab2da1e25d4f6ed1e6b9b9ed5e9d3bffed7
Alexey Shtele <aleksey.shtele@rt.ru>
2021-07-30 15:56:25 +0400

    Lesson10 HW10 fix reviewer notes

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -39,8 +39,9 @@ public class DataStreamSerializer implements StreamSerializer {
             Map<SectionType, Section> sections = r.getSections();
             dos.writeInt(sections.size());
             for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                writeSection(dos, entry.getValue());
+                SectionType sectionType = entry.getKey();
+                dos.writeUTF(sectionType.name());
+                writeSection(dos, sectionType, entry.getValue());
             }
         }
     }
@@ -59,88 +60,107 @@ public class DataStreamSerializer implements StreamSerializer {
 
             int sectSize = dis.readInt();
             for (int i = 0; i < sectSize; i++) {
-                resume.addSection(SectionType.valueOf(dis.readUTF()), readSection(dis));
+                SectionType sectionType = SectionType.valueOf(dis.readUTF());
+                resume.addSection(sectionType, readSection(dis, sectionType));
             }
             return resume;
         }
     }
 
-    private void writeSection(DataOutputStream dos, Section section) throws IOException {
-        dos.writeUTF(section.getClass().getName());
-        Class<? extends Section> clazz = section.getClass();
-        if (clazz == TextSection.class) {
-            TextSection textSection = (TextSection) section;
-            dos.writeUTF(textSection.getContent());
-        }
-        if (clazz == ListSection.class) {
-            ListSection listSection = (ListSection) section;
-            List<String> items = listSection.getItems();
-            dos.writeInt(items.size());
-            for (String item : items) {
-                dos.writeUTF(item);
-            }
-        }
-        if (clazz == OrganizationSection.class) {
-            OrganizationSection organizationSection = (OrganizationSection) section;
-            List<Organization> organizations = organizationSection.getOrganizations();
-            dos.writeInt(organizations.size());
-            for (Organization organization : organizations) {
-                Link link = organization.getHomePage();
-                dos.writeUTF(link.getName());
-                dos.writeUTF(link.getUrl());
-                List<Position> positions = organization.getPositions();
-                dos.writeInt(positions.size());
-                for (Position position : positions) {
-                    dos.writeUTF(position.getStartDate().toString());
-                    dos.writeUTF(position.getEndDate().toString());
-                    dos.writeUTF(position.getTitle());
-                    dos.writeUTF(position.getDescription());
+    private void writeSection(DataOutputStream dos, SectionType sectionType, Section section) throws IOException {
+        switch (sectionType) {
+            case PERSONAL:
+            case OBJECTIVE:
+                // TextSection
+                TextSection textSection = (TextSection) section;
+                dos.writeUTF(textSection.getContent());
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                // ListSection
+                ListSection listSection = (ListSection) section;
+                List<String> items = listSection.getItems();
+                dos.writeInt(items.size());
+                for (String item : items) {
+                    dos.writeUTF(item);
                 }
-            }
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                // OrganizationSection
+                OrganizationSection organizationSection = (OrganizationSection) section;
+                List<Organization> organizations = organizationSection.getOrganizations();
+                dos.writeInt(organizations.size());
+                for (Organization organization : organizations) {
+                    Link link = organization.getHomePage();
+                    dos.writeUTF(link.getName());
+                    String url = link.getUrl();
+                    dos.writeUTF(url == null ? "" : url);
+                    List<Position> positions = organization.getPositions();
+                    dos.writeInt(positions.size());
+                    for (Position position : positions) {
+                        writeLocalDate(dos, position.getStartDate());
+                        writeLocalDate(dos, position.getEndDate());
+                        dos.writeUTF(position.getTitle());
+                        String description = position.getDescription();
+                        dos.writeUTF(description == null ? "" : description);
+                    }
+                }
+                break;
         }
     }
 
-    private Section readSection(DataInputStream dis) throws IOException {
-        Class<?> clazz;
-        try {
-            clazz = Class.forName(dis.readUTF());
-        } catch (ClassNotFoundException e) {
-            throw new IllegalStateException(e);
-        }
-
-        if (clazz == TextSection.class) {
-            return new TextSection(dis.readUTF());
-        }
-
-        if (clazz == ListSection.class) {
-            List<String> items = new ArrayList<>();
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                items.add(dis.readUTF());
-            }
-            return new ListSection(items);
-        }
-        
-        if (clazz == OrganizationSection.class) {
-            List<Organization> organizations = new ArrayList<>();
-            int orgSize = dis.readInt();
-            for (int i = 0; i < orgSize; i++) {
-                Link link = new Link(dis.readUTF(), dis.readUTF());
-                List<Position> positions = new ArrayList<>();
-                int posSize = dis.readInt();
-                for (int j = 0; j < posSize; j++) {
-                    LocalDate startDate = LocalDate.parse(dis.readUTF());
-                    LocalDate endDate = LocalDate.parse(dis.readUTF());
-                    String title = dis.readUTF();
-                    String description = dis.readUTF();
-                    positions.add(new Position(startDate, endDate, title, description));
+    private Section readSection(DataInputStream dis, SectionType sectionType) throws IOException {
+        switch (sectionType) {
+            case OBJECTIVE:
+            case PERSONAL:
+                // TextSection
+                return new TextSection(dis.readUTF());
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                // ListSection
+                List<String> items = new ArrayList<>();
+                int size = dis.readInt();
+                for (int i = 0; i < size; i++) {
+                    items.add(dis.readUTF());
                 }
-                organizations.add(new Organization(link, positions));
-            }
-            return new OrganizationSection(organizations);
+                return new ListSection(items);
+            case EXPERIENCE:
+            case EDUCATION:
+                // OrganizationSection
+                List<Organization> organizations = new ArrayList<>();
+                int orgSize = dis.readInt();
+                for (int i = 0; i < orgSize; i++) {
+                    String name = dis.readUTF();
+                    String url = dis.readUTF();
+                    url = "".equals(url) ? null : url;
+                    Link link = new Link(name, url);
+                    List<Position> positions = new ArrayList<>();
+                    int posSize = dis.readInt();
+                    for (int j = 0; j < posSize; j++) {
+                        LocalDate startDate = readLocalDate(dis);
+                        LocalDate endDate = readLocalDate(dis);
+                        String title = dis.readUTF();
+                        String description = dis.readUTF();
+                        description = "".equals(description) ? null : description;
+                        positions.add(new Position(startDate, endDate, title, description));
+                    }
+                    organizations.add(new Organization(link, positions));
+                }
+                return new OrganizationSection(organizations);
+            default:
+                throw new IllegalStateException("Section not found");
         }
+    }
 
-        throw new IllegalStateException("Section not found");
+    private LocalDate readLocalDate(DataInputStream dis) throws IOException {
+        return LocalDate.of(dis.readInt(), dis.readInt(), dis.readInt());
+    }
+
+    private void writeLocalDate(DataOutputStream dos, LocalDate date) throws IOException {
+        dos.writeInt(date.getYear());
+        dos.writeInt(date.getMonthValue());
+        dos.writeInt(date.getDayOfMonth());
     }
 
 }
