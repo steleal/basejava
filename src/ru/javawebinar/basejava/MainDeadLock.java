4530c933e788edd5401b69f098a97f2f6ab6c7b7
Alexey Shtele <aleksey.shtele@rt.ru>
2021-08-03 20:50:02 +0400

    Lesson11 HW11 - deadlock with CyclicBarrier

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -1,66 +1,27 @@
 package ru.javawebinar.basejava;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class MainDeadLock {
-    public static void main(String[] args) throws InterruptedException {
-        Account accountA = new Account("A", 100);
-        Account accountB = new Account("B", 200);
+    private static CyclicBarrier barrier = new CyclicBarrier(2);
 
-        Thread successful = new Thread(new BadTransfer(accountA, accountB, 20));
-        successful.start();
-        successful.join();
-
-        Thread fromAToB = new Thread(new BadTransfer(accountA, accountB, 20));
-        Thread fromBToA = new Thread(new BadTransfer(accountB, accountA, 35));
-        fromAToB.start();
-        fromBToA.start();
-        fromAToB.join();
-        fromBToA.join();
-
-        System.out.println("If you see it - No deadlock!!!!");
+    public static void main(String[] args) {
+        Object objectA = new Object();
+        Object objectB = new Object();
+        new Thread(() -> deadlock(objectA, objectB)).start();
+        new Thread(() -> deadlock(objectB, objectA)).start();
     }
 
-    public static class Account {
-        private String name;
-        private int money;
-
-        public Account(String name, int money) {
-            this.name = name;
-            this.money = money;
-        }
-    }
-
-    public static class BadTransfer implements Runnable {
-        private Account source;
-        private Account destination;
-        private int transfer;
-
-        public BadTransfer(Account source, Account destination, int transfer) {
-            this.source = source;
-            this.destination = destination;
-            this.transfer = transfer;
-        }
-
-        @Override
-        public void run() {
-            String threadName = Thread.currentThread().getName();
-            System.out.printf("%s - start transfer %s$ from %s to %s%n", threadName, transfer, source.name, destination.name);
-            synchronized (source) {
-                delay();
-                synchronized (destination) {
-                    source.money -= transfer;
-                    destination.money += transfer;
-                }
-            }
-            System.out.printf("%s - transfer %s$ from %s to %s is successful!%n%n", threadName, transfer, source.name, destination.name);
-        }
-
-        private void delay() {
+    private static void deadlock(Object o1, Object o2) {
+        synchronized (o1) {
             try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
+                barrier.await();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+            synchronized (o2) {
+                System.out.println(Thread.currentThread().getName() + "- no deadlock");
+            }
         }
     }
-
 }
