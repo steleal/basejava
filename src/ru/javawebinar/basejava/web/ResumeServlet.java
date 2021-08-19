cc63070b1e7b9b04f4db77e56887a67f5c555581
Alexey Shtele <aleksey.shtele@rt.ru>
2021-08-19 11:12:05 +0400

    Lesson16 jsp

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
-import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
@@ -22,27 +20,15 @@ public class ResumeServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        кодировку не надо устанавливать, она ставится в list.jsp
+//        request.setCharacterEncoding("UTF-8");
+//        response.setCharacterEncoding("UTF-8");
+//        //  the same as .setHeader("Content-Type", "text/html; charset=UTF-8");
+//        response.setContentType("text/html; charset=UTF-8");
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
 
-        // the same as .setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-
-        response.getWriter().write(mapToTable(storage.getAllSorted()));
     }
 
-    private String mapToTable(List<Resume> resumes) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("<table>\n<tr><th>Uuid</th><th>Full Name</th></tr>\n");
-        for (Resume resume : resumes) {
-            builder.append("<tr><td>")
-                    .append(resume.getUuid())
-                    .append("</td><td>")
-                    .append(resume.getFullName())
-                    .append("</tr>\n");
-        }
-        builder.append("</table>");
-        return builder.toString();
-    }
 }
